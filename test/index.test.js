9d96814eabe9c82755d68bdaeab0904e771014b8
elimart12 <20211604@itla.edu.do>
2025-04-14 15:41:37 -0400

    mensaje de tests mejorado

--- test/index.test.js
+++ test/index.test.js
@@ -2,9 +2,11 @@ const fs = require('fs');
 const path = require('path');
 const { JSDOM } = require('jsdom');
 
-test('La página contiene "Hola Mundo"', () => {
+test('La página contiene "Hola Mundo 🌍"', () => {
   const html = fs.readFileSync(path.resolve(__dirname, '../index.html'), 'utf8');
   const dom = new JSDOM(html);
   const h1 = dom.window.document.querySelector('h1');
-  expect(h1.textContent).toBe('Hola Mundo');
+  expect(h1.textContent).toBe('Hola Mundo 🌍');
+
 });
+
